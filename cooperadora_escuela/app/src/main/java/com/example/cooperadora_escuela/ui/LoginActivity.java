01877c9dcd751d1d64304e50dc69ed54abc4c54b
Margarita-Bravo <embravo3329@gmail.com>
2025-05-21 15:07:19 -0300

    modificando boton

--- cooperadora_escuela/app/src/main/java/com/example/cooperadora_escuela/ui/LoginActivity.java
+++ cooperadora_escuela/app/src/main/java/com/example/cooperadora_escuela/ui/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.cooperadora_escuela.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.cooperadora_escuela.MainActivity;
 import com.example.cooperadora_escuela.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -11,6 +14,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnIrALogin = findViewById(R.id.btnLogin);
+        btnIrALogin.setOnClickListener(view -> {
+            Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+            startActivity(intent);
+        });
 
     }
 }
--- cooperadora_escuela/app/src/main/java/com/example/cooperadora_escuela/ui/RegisterActivity.java
+++ cooperadora_escuela/app/src/main/java/com/example/cooperadora_escuela/ui/RegisterActivity.java
@@ -1,9 +1,12 @@
 package com.example.cooperadora_escuela.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.cooperadora_escuela.MainActivity;
 import com.example.cooperadora_escuela.R;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -12,5 +15,10 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        Button btnIrARegister=findViewById(R.id.btnRegister);
+        btnIrARegister.setOnClickListener(view -> {
+            Intent intent = new Intent(RegisterActivity.this, DashboardActivity.class);
+            startActivity(intent);
+        });
     }
 }
